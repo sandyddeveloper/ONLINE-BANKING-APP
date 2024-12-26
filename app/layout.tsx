64f2c493e18dd38fdf412b7e1a3daf00ed58e676
sandyddeveloper <sandyddeveloper@gmail.com>
2024-12-26 06:46:14 +0000

    init

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+export const dynamic = "force-dynamic";
+
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-serif",
+});
+
+export const metadata: Metadata = {
+  title: "Vaultify ",
+  description:
+    "Experience secure, seamless, and innovative banking with Vaultify. Our platform empowers users with cutting-edge features, including real-time transactions, advanced security protocols, and intuitive financial tools. Manage your finances with confidence and ease, anytime and anywhere.",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
