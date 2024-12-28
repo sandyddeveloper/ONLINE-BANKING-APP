9a178d8c2556bf5badb3945c510c6b0e4e04b856
DEVxNET-ORG <devxnetoffl@gmail.com>
2024-12-28 21:40:56 +0530

    error repairing

--- lib/utils.ts
+++ lib/utils.ts
@@ -102,7 +102,6 @@ export interface AccountType {
   currentBalance: number;
 }
 
-export interface Bank extends AccountType {}
 export function formUrlQuery({ params, key, value }: UrlQueryParams) {
   const currentUrl = qs.parse(params);
 
@@ -150,24 +149,6 @@ export function countTransactionCategories(
 ): CategoryCount[] {
   const categoryCounts: { [category: string]: number } = {};
   let totalCount = 0;
-
-  // Iterate over each transaction
-  transactions &&
-    transactions.forEach((transaction) => {
-      // Extract the category from the transaction
-      const category = transaction.category;
-
-      // If the category exists in the categoryCounts object, increment its count
-      if (categoryCounts.hasOwnProperty(category)) {
-        categoryCounts[category]++;
-      } else {
-        // Otherwise, initialize the count to 1
-        categoryCounts[category] = 1;
-      }
-
-      // Increment total count
-      totalCount++;
-    });
 
   // Convert the categoryCounts object to an array of objects
   const aggregatedCategories: CategoryCount[] = Object.keys(categoryCounts).map(
