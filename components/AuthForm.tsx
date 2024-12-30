f8993d858c4fcff372d0aff232b295e58b0f8dc7
DEVxNET-ORG <devxnetoffl@gmail.com>
2024-12-30 14:10:03 +0530

    fully designed auth pages and create new userapi and implement appwrite concept in django

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -2,9 +2,37 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import CustomInput from "./Custominput";
+import { authFormSchema } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
 
 const AuthForm = ({ type }: { type: string }) => {
-  const [user] = useState(null);
+  const formScheme = authFormSchema(type);
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formScheme>>({
+    resolver: zodResolver(formScheme),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formScheme>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    setLoading(true);
+    console.log(values);
+    setLoading(false);
+  }
+
+  const [user, setuser] = useState(null);
+  const [isLoading, setLoading] = useState(false);
   return (
     <section className="auth-form">
       <header className="flex flex-col gap-3 md:gap-8">
@@ -33,7 +61,136 @@ const AuthForm = ({ type }: { type: string }) => {
       {user ? (
         <div className="flex flex-col gap-4">{/* PLAIDLINK */}</div>
       ) : (
-        <>FORM</>
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              {type === "sign-up" && (
+                <>
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name="firstName"
+                      label="First Name"
+                      placeholder="E.g., Santhosh"
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name="lastName"
+                      label="Last Name"
+                      placeholder="E.g., Raj"
+                    />
+                  </div>
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name="address"
+                      label="Address"
+                      placeholder="E.g., 123 MG Road, Flat No. 4B"
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name="city"
+                      label="City"
+                      placeholder="Enter your city"
+                    />
+                  </div>
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name="state"
+                      label="State"
+                      placeholder="E.g., Tamil Nadu"
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name="pincode"
+                      label="Pin Code"
+                      placeholder="E.g., 600001"
+                    />
+                  </div>
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name="dob"
+                      label="Date Of Birth"
+                      placeholder="DD-MM-YYYY"
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name="pancard"
+                      label="PAN Card"
+                      placeholder="E.g., ABCDE1234F"
+                    />
+                  </div>
+                  <CustomInput
+                    control={form.control}
+                    name="email"
+                    label="Email"
+                    placeholder="E.g., emailid@example.com"
+                  />
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name="password"
+                      label="Password"
+                      placeholder="Enter a strong password"
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name="conformpassword"
+                      label="Confirm Password"
+                      placeholder="Re-enter your password"
+                    />
+                  </div>
+                </>
+              )}
+              {type === "sign-in" && (
+                <>
+                  <CustomInput
+                    control={form.control}
+                    name="email"
+                    label="Email"
+                    placeholder="Enter your email"
+                  />
+                  <CustomInput
+                    control={form.control}
+                    name="password"
+                    label="Password"
+                    placeholder="Enter your password"
+                  />
+                </>
+              )}
+
+              <div className="flex flex-col gap-4">
+                <Button type="submit" disabled={isLoading} className="form-btn">
+                  {isLoading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" /> &nbsp;
+                      Loading...
+                    </>
+                  ) : type === "sign-in" ? (
+                    "Sign In"
+                  ) : (
+                    "Sign Up"
+                  )}
+                </Button>
+              </div>
+            </form>
+          </Form>
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600">
+              {type === "sign-in"
+                ? "Don't have an account"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="form-link"
+            >
+              {type === "sign-in" ? "Sign up" : "Sign in"}
+            </Link>
+          </footer>
+        </>
       )}
     </section>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 // import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
@@ -51,3 +52,45 @@ export function getAccountTypeColors(type: AccountTypes) {
       };
   }
 }
+
+export const authFormSchema = (type: string) =>
+  z.object({
+    email: z.string().email().max(50),
+    password: z.string().min(8).max(20),
+    conformpassword:
+      type === "sign-in" ? z.string().optional() : z.string().min(8).max(20),
+    pancard:
+      type === "sign-in"
+        ? z.string().optional()
+        : z
+            .string()
+            .regex(/^([A-Z]{5}[0-9]{4}[A-Z]{1})$/)
+            .min(10)
+            .max(10),
+    dob:
+      type === "sign-in"
+        ? z.string().optional()
+        : z
+            .string()
+            .regex(/^\d{2}\/\d{2}\/\d{4}$/)
+            .min(10)
+            .max(10),
+    pincode:
+      type === "sign-in"
+        ? z.string().optional()
+        : z
+            .string()
+            .regex(/^\d{6}$/)
+            .min(6)
+            .max(6),
+    state:
+      type === "sign-in" ? z.string().optional() : z.string().min(3).max(50),
+    address:
+      type === "sign-in" ? z.string().optional() : z.string().min(10).max(100),
+    lastName:
+      type === "sign-in" ? z.string().optional() : z.string().min(2).max(30),
+    firstName:
+      type === "sign-in" ? z.string().optional() : z.string().min(2).max(30),
+    city:
+      type === "sign-in" ? z.string().optional() : z.string().min(2).max(30),
+  });
